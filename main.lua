0e4a6803da867294cb3cfd58661ff28e4e103d97
Polly Nielsen Boutet-Livoff <pollyboutet@gmail.com>
2016-09-17 23:21:55 +0200

    change from list of objects to 2d list

--- main.lua
+++ main.lua
@@ -15,6 +15,8 @@ min_scale = 0.1
 local sel_tile = 1
 local tile_scale = 8
 
+local map = {}
+
 function love.load(main)
   camera = Camera:make(0, 0, 960 / love.graphics.getWidth(), 640 / love.graphics.getHeight(), 0)
 
@@ -24,6 +26,7 @@ function love.load(main)
   --load tiles
   level_editor:add_tile(0, 0, 0, "banker.png")
   level_editor:add_tile(255, 255, 0, "julian.png")
+
 end
 
 function love.update()
@@ -32,23 +35,14 @@ function love.update()
       temp_x, temp_y = camera:mousePosition()
       temp_x = (temp_x-temp_x%tile_scale)
       temp_y = (temp_y-temp_y%tile_scale)
-      for i, v in ipairs(level_editor.boxes) do
-        if temp_x == v.x and temp_y == v.y then
-          table.remove(level_editor.boxes, i)
-        end
-      end
-      level_editor:add_block(camera:mouseX()-camera:mouseX()%tile_scale, camera:mouseY()-camera:mouseY()%tile_scale, sel_tile)
+      level_editor:add_block((camera:mouseX()-camera:mouseX()%tile_scale)/tile_scale, (camera:mouseY()-camera:mouseY()%tile_scale)/tile_scale, sel_tile)
     end
   elseif love.mouse.isDown(2) then
     if love.mouse.getY() > tile_scale and camera:mouseX()>0 and camera:mouseY()>0 then
       temp_x, temp_y = camera:mousePosition()
       temp_x = (temp_x-temp_x%tile_scale)
       temp_y = (temp_y-temp_y%tile_scale)
-      for i, v in ipairs(level_editor.boxes) do
-        if temp_x == v.x and temp_y == v.y then
-          table.remove(level_editor.boxes, i)
-        end
-      end
+      level_editor:add_block((camera:mouseX()-camera:mouseX()%tile_scale)/tile_scale, (camera:mouseY()-camera:mouseY()%tile_scale)/tile_scale, nil)
     end
   elseif love.mouse.isDown(3) then
     camera:move((level_editor.oldX-love.mouse.getX()) * camera.scaleX, (level_editor.oldY-love.mouse.getY()) * camera.scaleY)
@@ -58,9 +52,6 @@ function love.update()
 end
 
 function love.draw()
-  love.graphics.setColor(255, 0, 100)
-  love.graphics.printf(level_editor.map_name, 0, 0, love.graphics.getWidth())
-
   camera:set()
   love.graphics.setBackgroundColor(255, 255, 255)
   love.graphics.setColor(0, 0, 0, 122)
@@ -75,14 +66,19 @@ function love.draw()
 
   love.graphics.setColor(255, 255, 255, 255)
 
-  for i, v in ipairs(level_editor.boxes) do
-    love.graphics.draw(v.image, v.x, v.y)
+  for x, xv in pairs(map) do
+    for y, v in pairs(xv) do
+      love.graphics.draw(level_editor.tiles[v].image, x*tile_scale-tile_scale, y*tile_scale-tile_scale)
+    end
   end
 
   love.graphics.setColor(255, 0, 255, 200)
   love.graphics.draw(level_editor.tiles[sel_tile].image, camera:mouseX()-camera:mouseX()%tile_scale, camera:mouseY()-camera:mouseY()%tile_scale)
 
   camera:unset()
+
+  love.graphics.setColor(255, 0, 100)
+  love.graphics.printf(level_editor.map_name, 0, 0, love.graphics.getWidth())
 end
 
 function love.wheelmoved(x, y)
@@ -121,8 +117,6 @@ function love.textinput(t)
 end
 
 function love.keypressed(key, scancode, isrepeat)
-
-
   if key == "s" and not isrepeat then
     level_editor:save_map()
   elseif key == "o" and not isrepeat and not level_editor.is_loading_map then
@@ -161,27 +155,36 @@ function level_editor:add_tile(r, g, b, image)
 end
 
 function level_editor:add_block(x, y, id)
-  table.insert(level_editor.boxes, {image = level_editor.tiles[id].image, x = x, y = y, width = level_editor.tiles[id].width, height = level_editor.tiles[id].height, r=level_editor.tiles[id].r, g=level_editor.tiles[id].g, b=level_editor.tiles[id].b})
+  x = x+1
+  y = y+1
+  if not map[x] then
+    map[x] = {}
+  end
+  map[x][y] = id
 end
 
 function level_editor:save_map()
-  maxX, maxY = 1,1
-  for i,v in ipairs(level_editor.boxes) do
-    if v.x/tile_scale>maxX then
-      maxX = v.x/tile_scale
-    end
-    if v.y/tile_scale>maxY then
-      maxY = v.y/tile_scale
+  maxX, maxY = 1, 1
+  for x, xv in pairs(map) do
+    for y, v in pairs(xv) do
+      if x>maxX then
+        maxX = x
+      end
+      if y>maxY then
+        maxY = y
+      end
     end
   end
-  tempLevel = love.image.newImageData(maxX+1, maxY+1)
+  tempLevel = love.image.newImageData(maxX, maxY)
   for x = 1, maxX do
     for y = 1, maxY do
-      tempLevel:setPixel(x, y, 255, 255, 255)
+      tempLevel:setPixel(x-1, y-1, 255, 255, 255)
     end
   end
-  for i,v in ipairs(level_editor.boxes) do
-    tempLevel:setPixel(v.x/tile_scale, v.y/tile_scale, v.r, v.g, v.b, v.a)
+  for x, xv in pairs(map) do
+    for y, v in pairs(xv) do
+      tempLevel:setPixel(x-1, y-1, level_editor.tiles[v].r, level_editor.tiles[v].g, level_editor.tiles[v].b)
+    end
   end
   if not love.filesystem.exists("maps") then
     love.filesystem.createDirectory("maps")
@@ -191,19 +194,15 @@ function level_editor:save_map()
 end
 
 function level_editor:load_map(image_data)
-  temp_boxes = {}
+  map = {}
   for x=1,image_data:getWidth() do
     for y=1,image_data:getHeight() do
       for i=1, table.getn(level_editor.tiles) do
         r, g, b, a = image_data:getPixel(x-1, y-1)
         if r == level_editor.tiles[i].r and g == level_editor.tiles[i].g and b == level_editor.tiles[i].b then
-          print(b)
-          table.insert(temp_boxes, {image = level_editor.tiles[i].image, x = x*tile_scale-tile_scale, y = y*tile_scale-tile_scale, width = tile_scale, height = tile_scale, r=r, g=g, b=b, a=a})
+          level_editor:add_block(x-1, y-1, i)
         end
       end
     end
   end
-  level_editor.boxes = temp_boxes
 end
-
-return level_editor
